c7ddad5682938d1f1f5db6a3041a6ca4f7f88832
lizhe <lizhe@dogesoft.cn>
2025-12-22 03:10:32 +0000

    Week1-Day1-NumPy Basics

---
+++ day1_numpy_basics.py
@@ -0,0 +1,18 @@
+#!usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author: Lizhe
+# @Time: 2025/12/22 12:29 上午
+import sys
+import cv2, numpy as np
+
+# Day 1 – NumPy Basics
+# - Understand image as a NumPy array
+# - Practice slicing (ROI)
+# - Simulate grayscale and color images
+
+print("Python:", sys.version)
+print("NumPy:", np.__version__)
+print("OpenCV:", cv2.__version__)
+print("Python path:", sys.executable)
+
+
